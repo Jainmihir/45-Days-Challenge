c81ce2777457ffbde0a7dbf7d7282105dd3894cc
Mihir Jain <jainsuresh713@gmail.com>
2024-05-18 11:03:44 +0530

    Counter Project

--- Mern/NodeJs/07-ShortUrl/service/auth.js
+++ Mern/NodeJs/07-ShortUrl/service/auth.js
@@ -1,12 +1,28 @@
-const sessionIdToUserMap = new Map();
+const jwt = require('jsonwebtoken')
+const secret = "Mihir@1234";
 
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user);
+function setUser(user){
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    },secret);
 }
 
-function getUser(id,user){
-    return sessionIdToUserMap.get(id);
+
+function getUser(token){
+    if(!token) return null;
+    return jwt.verify(token,secret);
 }
+
+// const sessionIdToUserMap = new Map();
+
+// function setUser(id,user){
+//     sessionIdToUserMap.set(id,user);
+// }
+
+// function getUser(id,user){
+//     return sessionIdToUserMap.get(id);
+// }
 
 module.exports = {
     setUser,
--- Mern/React-Js/02Counter-Project/src/App.jsx
+++ Mern/React-Js/02Counter-Project/src/App.jsx
@@ -12,20 +12,20 @@ function App() {
   
 
   const addValue = ()=>{
-    if(counter  < 20 ){
+    // if(counter  < 20 ){
       // counter = counter + 1;
       // counter = counter + 1;
       // counter = counter + 1;
     // console.log(counter);
       setCounter((prevCounter) => prevCounter+1);
-      setCounter((prevCounter) => prevCounter+1);
-      setCounter((prevCounter) => prevCounter+1);
-      setCounter((prevCounter) => prevCounter+1);
+      // setCounter((prevCounter) => prevCounter+1);
+      // setCounter((prevCounter) => prevCounter+1);
+      // setCounter((prevCounter) => prevCounter+1);
       // usestate -> react fibre ki wjh se counter ko batches ki form me bhejta h isliye woh ek sath counter ko update nhi kr paate 
       // setCounter(counter + 1);
       // setCounter(counter + 1);
       // setCounter(counter + 1);
-    }
+    // }
   }
   // const addValue = document.querySelector('#onclick')
   // addValue.addEventListener('click',(e)=>{
@@ -37,19 +37,24 @@ function App() {
   // });
 
   const removeValue = () =>{
-    if(counter > 0){
-      counter = counter-1;
-      setCounter(counter);
-    }
+    // if(counter > 0){
+      // counter = counter-1;
+      setCounter(counter-1);
+    // }
+  }
+  const resetValue = () =>{
+    setCounter(0);
   }
 
   return (
     <>
-    <h1>Hello World !</h1>
+    <h1>Tiftu Project Counter !</h1>
     <h2>Counter Value : {counter} </h2>
     <button onClick={addValue}> Add Value :{counter} </button>
     <br/>
     <button onClick={removeValue}> Remove Value :{counter}</button>
+    <br/>
+    <button onClick={resetValue}> Reset Value :{counter}</button>
     </>
   )
 }
